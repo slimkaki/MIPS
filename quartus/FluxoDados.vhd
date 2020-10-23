07ec2ff65086e1c0760d7f1894e2af2ad3006c6d
slimkaki <rafaalmada98@gmail.com>
2020-10-23 15:16:02 -0300

    Fetch pronto!

---
+++ quartus/FluxoDados.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity FluxoDados is
+    generic();
+    port();
+end entity;
+
+architecture of FluxoDados is
+    begin
+
+end architecture;

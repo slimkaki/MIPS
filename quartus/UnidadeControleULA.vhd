17dc368091978daba4cbf479a8787197c81b291f
heliopn <hpaivan@gmail.com>
2020-12-09 01:28:18 -0300

    Agora vai

--- quartus/UnidadeControleULA.vhd
+++ quartus/UnidadeControleULA.vhd
@@ -26,18 +26,16 @@ begin
 	 
     ULActrl <=   "010" WHEN (opcode = add_R(11 downto 6) and funct = add_R(5 downto 0) and (ULAop = "10")) or 
                             (opcode = addu_R(11 downto 6) and funct = addu_R(5 downto 0) and (ULAop = "10")) or
-                            (opcode = addi_I(11 downto 6) and (ULAop = "10")) ELSE
+                            (opcode = addi_I(11 downto 6) and (ULAop = "10")) or (ULAop = "00") ELSE
                  "110" WHEN (opcode = sub_R(11 downto 6) and funct = sub_R(5 downto 0) and (ULAop = "10")) or
                             (opcode = subu_R(11 downto 6) and funct = subu_R(5 downto 0) and (ULAop = "10")) or
-                            (opcode = beq_I(11 downto 6) and (ULAop = "10")) or (opcode = bne_I(11 downto 6) and (ULAop = "10")) ELSE
+                            (opcode = beq_I(11 downto 6) and (ULAop = "10")) or (opcode = bne_I(11 downto 6) and (ULAop = "10")) or (ULAop = "01") ELSE
                  "111" WHEN (opcode = slt_r(11 downto 6) and funct = slt_R(5 downto 0) and (ULAop = "10")) or
                             (opcode = sltu_R(11 downto 6) and funct = sltu_R(5 downto 0) and (ULAop = "10")) or 
 									 (opcode = slti_I(11 downto 6) and (ULAop = "10")) ELSE
                  "000" WHEN (opcode = and_R(11 downto 6) and funct = and_R(5 downto 0) and (ULAop = "10")) or
                             (opcode = andi_I(11 downto 6) and (ULAop = "10")) ELSE
                  "001" WHEN (opcode = or_R(11 downto 6) and funct = or_R(5 downto 0) and (ULAop = "10")) or
-                            (opcode = ori_I(11 downto 6) and (ULAop = "10")) ELSE
-                 "110" WHEN (ULAop = "01") ELSE
-                 "010" WHEN (ULAop = "00") ;
+                            (opcode = ori_I(11 downto 6) and (ULAop = "10"));
 
 end architecture;
